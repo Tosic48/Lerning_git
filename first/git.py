3ebac57f29fb761889eae693edd5888bf75e1baf
AntonWPC <antonsuhodolski@tut.by>
2023-01-03 15:33:10 +0300

    hi Vas

--- first/git.py
+++ first/git.py
@@ -2,16 +2,15 @@ def plus(a, b):
     return a + b
 
 
+def minus(a, b):
+    return a - b
 
-def minus(a,b):
-    return a-b
 
-
-def mul(a,b):
-    return a*b
+def mul(a, b):
+    return a * b
 
 
 if __name__ == '__main__':
     print(plus(2, 3))
-    print(minus(4,2))
-    print(mul(3,3))
+    print(minus(4, 2))
+    print(mul(3, 3))
